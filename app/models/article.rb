f5a91b5ca16f1d2fc06566d493781b17df8ac753
brandonoakley44 <brandonoakley324@gmail.com>
2020-02-24 00:37:32 -0400

    create new template , action, and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,7 @@
-class Article < ApplicationRecord
+class Article < ActiveRecord::Base 
 
-    #rails is cool in the sense that once your model is created you dont really have to 
-    #do much because your getters and setters are supplied 
 
-    #enforce some constraints on articles 
-    validates :title, presence: true, length: { minimum: 3, maximum: 50}
-    validates :description , presence: true , length: { minimum: 50, maximum: 300 }
-
+    validates :title, presence: true , length: { minimum: 3, maximum: 50 }
+    validates :description , presence: true , length: { minimum: 10, maximum: 300 }
 
 end
